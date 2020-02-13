b2139130013f42fd0fe86addd14ca00300cc39ef
k-nicha <45272352+k-nicha@users.noreply.github.com>
2020-02-13 21:57:28 +0100

    advanced checks

--- src/routes.js
+++ src/routes.js
@@ -43,6 +43,18 @@ function routes (server, localStorage) {
         if (!data) {
             res.status(400).json('Bad request, no data found.')
         } else {
+                 
+            const found = localStorage.find(element => {
+                return data.isbn === element.isbn // returns true or false
+            })
+
+            if (found) { // ISBN-ot sto probuvas da go zacuvas vekje postoi
+                res.status(200).json('This ISBN already exists')
+                return
+            }
+            
+            // Ako ISBN-ot ne postoi, vnesi go 
+            data.creationDate = new Date()
             localStorage.push(data)
             console.log(localStorage)
             res.status(201).json('Succesfully created the entry.')
@@ -66,6 +78,37 @@ function routes (server, localStorage) {
             }
             
         }
+    })
+
+    server.get('/books/get-todays-books', (req, res) => {
+        let newArray = []
+        const today = new Date()
+        // d/m/y
+        const shortDate = today.getDate() + '/' + today.getMonth() + '/' + today.getFullYear()
+        for (i=0; i < localStorage.length; i++) {
+            // book creation date
+            const longDate = localStorage[i].creationDate
+
+            if (longDate && (typeof longDate.getMonth === 'function')) {
+                const bookDate = longDate.getDate() + '/' + longDate.getMonth() + '/' + longDate.getFullYear()
+
+                if (shortDate === bookDate) {
+                    newArray.push(localStorage[i])
+                }
+            }
+        }
+        res.status(200).json(newArray)
+    })
+
+
+    server.get('/books/get-books-byauthor/:author', (req, res) => {
+        let newArray = []
+        
+        newArray = localStorage.filter((element) => {
+            return element.author === req.params.author 
+        })
+
+        res.status(200).json(newArray)
     })
 }
 
